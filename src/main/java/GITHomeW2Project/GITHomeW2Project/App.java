3ff35c2b2955944b0f3f96b61939a6e5e87d44e0
Marix2020 <maricelocampo888@gmail.com>
2020-07-03 17:26:23 -0400

    HomeWork Practice2

---
+++ src/main/java/GITHomeW2Project/GITHomeW2Project/App.java
@@ -0,0 +1,13 @@
+package GITHomeW2Project.GITHomeW2Project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
